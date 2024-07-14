365cfb615e87ab7b81fb5f754187d9c5ef920bee
Nutti Saelor <leelorz6@gmail.com>
2024-07-14 15:37:20 +0700

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+const host = 'api.frankfurter.app';
+
+type Currency = "USD" | "JPY" | "THB"
+
+interface CurrencyResult {
+    amount: number
+    base: string
+    date: string
+    rates: { [a: string]: number }
+}
+
+const convertCurrent = ({ amount, from, to }:
+    { amount: number, from: Currency, to: Currency }) => {
+    return fetch(`https://${host}/latest?amount=${amount}&from=${from}&to=${to}`)
+        .then(x => x.json() as any as CurrencyResult)
+}
+
+const main = async () => {
+    const a = await convertCurrent({
+        amount: 100,
+        from: "THB",
+        to: "USD"
+    })
+    console.log(a.rates.USD)
+}
+
+main()
+
