fbcf926312b0cd6e0d6cbfcee410f187d4b98b3b
Nutti Saelor <nutti.sae@truedigital.com>
2024-07-22 20:03:02 +0700

    feat: add nodemon ts-node

--- index.ts
+++ index.ts
@@ -1,28 +1,35 @@
-const host = 'api.frankfurter.app';
+const host = "api.frankfurter.app";
 
-type Currency = "USD" | "JPY" | "THB"
+type Currency = "USD" | "JPY" | "THB";
 
 interface CurrencyResult {
-    amount: number
-    base: string
-    date: string
-    rates: { [a: string]: number }
+  amount: number;
+  base: string;
+  date: string;
+  rates: { [a: string]: number };
 }
 
-const convertCurrent = ({ amount, from, to }:
-    { amount: number, from: Currency, to: Currency }) => {
-    return fetch(`https://${host}/latest?amount=${amount}&from=${from}&to=${to}`)
-        .then(x => x.json() as any as CurrencyResult)
-}
+const convertCurrent = ({
+  amount,
+  from,
+  to,
+}: {
+  amount: number;
+  from: Currency;
+  to: Currency;
+}) => {
+  return fetch(
+    `https://${host}/latest?amount=${amount}&from=${from}&to=${to}`
+  ).then((x) => x.json() as any as CurrencyResult);
+};
 
 const main = async () => {
-    const a = await convertCurrent({
-        amount: 100,
-        from: "THB",
-        to: "USD"
-    })
-    console.log(a.rates.USD)
-}
+  const a = await convertCurrent({
+    amount: 100,
+    from: "THB",
+    to: "USD",
+  });
+  console.log(a.rates);
+};
 
-main()
-
+main();
