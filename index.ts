317cd05ff5efea9aa1568ed244321b4c8b147178
Nutti Saelor <nutti.sae@truedigital.com>
2024-07-22 23:29:36 +0700

    feat: add cache

--- index.ts
+++ index.ts
@@ -1,23 +1,38 @@
-interface CurrencyResult {
+interface CurrencyResult<T extends string> {
   amount: number;
   base: string;
   date: string;
-  rates: { [a: string]: number };
+  rates: Record<T, number>;
 }
 
-class Currency<const Currencies> {
+class Currency<const Currencies extends string[]> {
   host = "api.frankfurter.app";
+  private caches: Record<string, number> = {};
 
   constructor(public currencies: Currencies) {}
 
-  convert(
+  async convert<To extends Currencies[keyof Currencies]>(
     from: Currencies[keyof Currencies],
-    to: Currencies[keyof Currencies],
+    to: To,
     amount: number
   ) {
-    return fetch(
+    const key = `${from}{to}` as To extends string ? To : never;
+
+    if (this.caches[key]) {
+      return this.caches[key];
+    }
+
+    const result = await fetch(
       `https://${this.host}/latest?amount=${amount}&from=${from}&to=${to}`
-    ).then((x) => x.json() as any as CurrencyResult);
+    ).then(
+      (x) => x.json() as any as CurrencyResult<To extends string ? To : never>
+    );
+
+    const rate = result.rates[to as To extends string ? To : never];
+
+    this.caches[key] = rate;
+
+    return rate;
   }
 
   get latest() {
@@ -29,4 +44,4 @@ const myCurrency = new Currency(["USD", "JPY", "THB"]);
 
 myCurrency.convert("THB", "USD", 100).then(console.log);
 
-myCurrency.latest.then(console.log);
+// myCurrency.latest.then(console.log);
