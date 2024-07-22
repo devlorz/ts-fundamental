7d2f2c55dadbb781819f29c0dbc738c7bc4f1362
Nutti Saelor <nutti.sae@truedigital.com>
2024-07-23 00:38:40 +0700

    feat: add namespace

--- index.ts
+++ index.ts
@@ -1,64 +1,66 @@
-interface CurrencyResult<T extends string> {
-  amount: number;
-  base: string;
-  date: string;
-  rates: Record<T, number>;
-}
-
-function cache(method: Function, context: any) {
-  return async function (
-    this: Currency,
-    from: string,
-    to: string,
-    amount: number
-  ) {
-    const key = `${from}${to}`;
-
-    if (this.caches[key]) return this.caches[key] * amount;
-
-    const result = await method.call(this, from, to, amount);
-
-    const rate = result.rates[to];
-    this.caches[key] = rate / result.amount;
-
-    return rate;
-  };
-}
-
-class Currency<
-  const Currencies extends readonly string[] = [],
-  Values extends string = Extract<Currencies[keyof Currencies], string>
-> {
-  host = "api.frankfurter.app";
-  caches: Record<string, number> = {};
-
-  constructor(public currencies: Currencies) {}
-
-  @cache
-  async convert<To extends Values>(from: Values, to: To, amount: number) {
-    return fetch(
-      `https://${this.host}/latest?amount=${amount}&from=${from}&to=${to}`
-    ).then((x) => x.json() as unknown as CurrencyResult<To>);
+namespace Currency {
+  interface CurrencyResult<T extends string> {
+    amount: number;
+    base: string;
+    date: string;
+    rates: Record<T, number>;
   }
 
-  get latest() {
-    return fetch(`https://${this.host}/latest`).then((x) => x.json());
+  function cache(method: Function, context: any) {
+    return async function (
+      this: Converter,
+      from: string,
+      to: string,
+      amount: number
+    ) {
+      const key = `${from}${to}`;
+
+      if (this.caches[key]) return this.caches[key] * amount;
+
+      const result = await method.call(this, from, to, amount);
+
+      const rate = result.rates[to];
+      this.caches[key] = rate / result.amount;
+
+      return rate;
+    };
+  }
+
+  class Converter<
+    const Currencies extends readonly string[] = [],
+    Values extends string = Extract<Currencies[keyof Currencies], string>
+  > {
+    protected host = "api.frankfurter.app";
+    protected caches: Record<string, number> = {};
+
+    constructor(public currencies: Currencies) {}
+
+    @cache
+    async convert<To extends Values>(from: Values, to: To, amount: number) {
+      return fetch(
+        `https://${this.host}/latest?amount=${amount}&from=${from}&to=${to}`
+      ).then((x) => x.json() as unknown as CurrencyResult<To>);
+    }
+
+    get latest() {
+      return fetch(`https://${this.host}/latest`).then((x) => x.json());
+    }
+  }
+
+  export class MyConverter<
+    const Currencies extends readonly string[] = [],
+    Values extends string = Extract<Currencies[keyof Currencies], string>
+  > extends Converter<Currencies, Values> {
+    constructor(
+      public currencies: Currencies,
+      public host = "api.frankfurter.app"
+    ) {
+      super(currencies);
+    }
   }
 }
 
-class MyCurrency<
-  const Currencies extends readonly string[] = [],
-  Values extends string = Extract<Currencies[keyof Currencies], string>
-> extends Currency<Currencies, Values> {
-  constructor(
-    public currencies: Currencies,
-    public host = "api.frankfurter.app"
-  ) {
-    super(currencies);
-  }
-}
-
-const myCurrency = new MyCurrency(["USD", "JPY", "THB"]);
+const myCurrency = new Currency.MyConverter(["USD", "JPY", "THB"]);
 
 const a = async () => {
   await myCurrency.convert("THB", "USD", 100).then(console.log);
